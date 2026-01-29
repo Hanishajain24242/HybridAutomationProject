c30e7569a0fd36ec857f7bce2861037de34a9269
Hanishaa Jain <hanishajain49@gmail.com>
2026-01-29 18:29:40 +0530

    Add files via upload

--- BaseTest.java
+++ BaseTest.java
@@ -1,19 +1,24 @@
-package org.selenium.automation.SeleniumAutomation;
+package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import config.ConfigReader;
+import drivers.driverfactory;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
-  
+    @BeforeMethod
     public void setup() {
-    	 WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com");
+        String browser = ConfigReader.getProperty("browser");
+        driver = driverfactory.initDriver(browser);
+        driver.get(ConfigReader.getProperty("url"));
     }
 
-   
+    @AfterMethod
     public void tearDown() {
         driver.quit();
     }
