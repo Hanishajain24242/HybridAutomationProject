96a6f3eadd8f036bcbf8d450472936ce8dddaf3c
Hanishaa Jain <hanishajain49@gmail.com>
2026-01-29 18:47:41 +0530

    Add files via upload

---
+++ WaitUtils.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtils {
+
+    public static WebElement waitForElementVisible(
+            WebDriver driver, By locator, int timeInSeconds) {
+
+        WebDriverWait wait = new WebDriverWait(
+                driver, Duration.ofSeconds(timeInSeconds));
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void waitForElementClickable(
+            WebDriver driver, By locator, int timeInSeconds) {
+
+        WebDriverWait wait = new WebDriverWait(
+                driver, Duration.ofSeconds(timeInSeconds));
+        wait.until(
+                ExpectedConditions.elementToBeClickable(locator));
+    }
+}
